718450fdbe637e02681aac5dab5571f3378aca9a
wlad9 <zav8b9@gmail.com>
2024-01-23 20:50:32 -0300

    Entidades Category-Order-Payment-Product-user  CRIADAS - atualização 2301212050P

---
+++ src/main/java/com/dev/dscommerce_vx/DscommerceVxApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dscommerce_vx;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceVxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DscommerceVxApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dev/dscommerce_vx/DscommerceVxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dscommerce_vx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceVxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
