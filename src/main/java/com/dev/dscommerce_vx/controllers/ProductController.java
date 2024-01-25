7bbb02617ccfb6ba771a49b19a668851abec5592
wlad9 <zav8b9@gmail.com>
2024-01-25 10:24:48 -0300

    Busca Paginada ok

--- src/main/java/com/dev/dscommerce_vx/controllers/ProductController.java
+++ src/main/java/com/dev/dscommerce_vx/controllers/ProductController.java
@@ -1,16 +1,14 @@
 package com.dev.dscommerce_vx.controllers;
 
 import com.dev.dscommerce_vx.dto.ProductDTO;
-import com.dev.dscommerce_vx.entities.Product;
-import com.dev.dscommerce_vx.repositories.ProductRepository;
 import com.dev.dscommerce_vx.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,5 +19,9 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id){
         ProductDTO dto = service.findById(id);
         return dto;
+    }
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 }
