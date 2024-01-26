b734d96ff73065816297c2d8c83fcc3bdfbc762f
wlad9 <zav8b9@gmail.com>
2024-01-25 21:35:32 -0300

    Bean validation

--- src/main/java/com/dev/dscommerce_vx/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/dev/dscommerce_vx/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.dev.dscommerce_vx.controllers.handlers;
 
 import com.dev.dscommerce_vx.dto.CustomError;
+import com.dev.dscommerce_vx.dto.ValidationError;
 import com.dev.dscommerce_vx.exceptions.DatabaseException;
 import com.dev.dscommerce_vx.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +26,15 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for(FieldError f: e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
