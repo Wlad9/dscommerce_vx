1cf6738a75bb1a83237623157163397a551acd20
wlad9 <zav8b9@gmail.com>
2024-01-25 21:08:44 -0300

    Tratamento de Exceções ok

--- src/main/java/com/dev/dscommerce_vx/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/dev/dscommerce_vx/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.dev.dscommerce_vx.controllers.handlers;
 
 import com.dev.dscommerce_vx.dto.CustomError;
+import com.dev.dscommerce_vx.exceptions.DatabaseException;
 import com.dev.dscommerce_vx.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,12 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/dev/dscommerce_vx/dto/ProductDTO.java
+++ src/main/java/com/dev/dscommerce_vx/dto/ProductDTO.java
@@ -3,11 +3,20 @@ package com.dev.dscommerce_vx.dto;
 import com.dev.dscommerce_vx.entities.Product;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+    @Size(min=3, max=80, message = "O nome deve ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido.")
     private String name;
+    @Size(min=10, message = "A descrição deve ter no mínimo 10 caracteres.")
+    @NotBlank
     private String description;
+    @Positive(message = "O preço deve ser positivo.")
     private Double price;
     private String imgUrl;
 
